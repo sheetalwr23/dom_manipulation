4800349823094aabffb3d57c88e3a55bcdd6980e
Sheetal <Sheetalwr23@gmail.com>
2023-03-12 15:12:49 +0530

    new things added

--- dom.js
+++ dom.js
@@ -43,7 +43,7 @@ var li = document.getElementsByTagName("li");
 // list[0].style.fontWeight = "bold";
 
 li[3].innerHTML = "hello";
-li[2].style.backgroundColor = "green";
+// li[2].style.backgroundColor = "green";
 
 for (let i = 0; i < li.length; i++) {
   li[i].style.fontWeight = "bold";
@@ -56,24 +56,28 @@ input.value = "hello world";
 
 var submit = document.querySelector('input[type="submit"]');
 submit.value = "send";
-submit.style.backgroundColor = "pink";
+// submit.style.backgroundColor = "pink";
 //~~~~~~~child member
-var lastItem = document.querySelector(".list-group-item:last-child");
-lastItem.style.color = "blue";
+// var lastItem = document.querySelector(".list-group-item:last-child");
+// lastItem.style.color = "blue";
 
 //~~~~Css pseudo selectors
-var secondItem = document.querySelector(".list-group-item:nth-child(1)");
-secondItem.style.backgroundColor = "skyblue";
+// var secondItem = document.querySelector(".list-group-item:nth-child(1)");
+// secondItem.style.backgroundColor = "skyblue";
 
 //~~~~~~~~querrySelectorAll
-
+//~~~~assignmnet 37 to end
 var titles = document.querySelectorAll(".title");
 titles[0].innerText = "my items";
 
-var odd = document.querySelectorAll("li:nth-child(odd)");
-var even = document.querySelectorAll("li:nth-child(even)");
+// var odd = document.querySelectorAll("li:nth-child(odd)");
+// var even = document.querySelectorAll("li:nth-child(even)");
 
-for (let i = 0; i < odd.length; i++) {
-  odd[i].style.backgroundColor = "red";
-  even[i].style.backgroundColor = "blue";
-}
+// for (let i = 0; i < odd.length; i++) {
+//   odd[i].style.backgroundColor = "red";
+//   even[i].style.backgroundColor = "blue";
+// }
+//~~~~~~~~~~assignment 30 to 37min
+var li = document.getElementsByClassName("list-group-item");
+li[1].style.backgroundColor = "green";
+li[2].style.visibility = "hidden";
